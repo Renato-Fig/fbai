3300d71ef86e49414eabaf196fdc5a6a16511e16
Renato-Fig <renato.fn10@gmail.com>
2023-10-09 00:52:12 -0300

    mudanca nas cores e copy

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -25,7 +25,7 @@ export function LandingPage() {
     return (
         <div id='landing-page'>
             <header id='navigation'>
-                <img src={LogoNavigation} alt="FB" />
+                <img src={LogoNavigation} alt="Enem Guru" />
                 <nav className='page-navigation'>
                     <ul>
                         <li><a href="#unique-selling-proposition">O que é?</a></li>
@@ -34,7 +34,7 @@ export function LandingPage() {
                         <li><a href="#call-to-action">Como usar?</a></li>
                     </ul>
                 </nav>
-                <a href="https://api.whatsapp.com/send?phone=558588686378" target='blank'><button className='primary-button'>Experimente agora</button></a>
+                <a href="https://api.whatsapp.com/send?phone=5585988686378&text=Olá%20Guru!%20Me%20passa%20umas%20dicas%20sobre%20o%20que%20eu%20devo%20estudar%20nessa%20reta%20final%20para%20o%20ENEM!%20" target='blank'><button className='primary-button'>Experimente agora</button></a>
 
                 <i className='menu-burger-button' onClick={toggleMenu}><FaBars /></i>
 
@@ -51,7 +51,7 @@ export function LandingPage() {
                                 <li><a href="#features" onClick={toggleMenu}>O que faz?</a></li>
                                 <li><a href="#closing-argument" onClick={toggleMenu}>Por que usar?</a></li>
                                 <li><a href="#call-to-action" onClick={toggleMenu}>Como usar?</a></li>
-                                <li><a href="https://api.whatsapp.com/send?phone=558588686378" target='blank'><button className='primary-btn'>Experimente agora</button></a></li>
+                                <li><a href="https://api.whatsapp.com/send?phone=5585988686378&text=Olá%20Guru!%20Me%20passa%20umas%20dicas%20sobre%20o%20que%20eu%20devo%20estudar%20nessa%20reta%20final%20para%20o%20ENEM!%20" target='blank'><button className='primary-btn'>Experimente agora</button></a></li>
                             </ul>
                         </nav>
                     </div>
@@ -61,9 +61,9 @@ export function LandingPage() {
 
             <section id='hero'>
                 <div className="content">
-                    <h1>Sua nova assistente<br />pessoal de IA para<br /><em>a prova do Enem</em></h1>
-                    <p>A FBAI é uma inteligência artificial treinada exclusivamente para responder a perguntas sobre o Enem, escrever resumos, fornecer questões e dar dicas de estudos, tudo diretamente no seu WhatsApp. É fácil e simples de usar.</p>
-                    <a href="https://api.whatsapp.com/send?phone=558588686378" target='blank'><button>Experimente agora</button></a>
+                    <h1>Seu novo assistente<br />pessoal de IA para<br /><em>a prova do Enem</em></h1>
+                    <p>O Enem Guru é uma inteligência artificial treinada exclusivamente para responder a perguntas sobre o Enem, escrever resumos, fornecer questões e dar dicas de estudos, tudo diretamente no seu WhatsApp. É fácil e simples de usar.</p>
+                    <a href="https://api.whatsapp.com/send?phone=5585988686378&text=Olá%20Guru!%20Me%20passa%20umas%20dicas%20sobre%20o%20que%20eu%20devo%20estudar%20nessa%20reta%20final%20para%20o%20ENEM!%20" target='blank'><button>Experimente agora</button></a>
                 </div>
                 <div className="image">
                     <img src={HeroImage} alt="" />
@@ -77,31 +77,31 @@ export function LandingPage() {
                 <div className="content">
                     <h3>O que é?</h3>
                     <h2>Já pensou estudar para o <em>Enem no Whatsapp?</em></h2>
-                    <p>Agora é possível com A FBAI. A preparação para o Enem nunca foi tão conveniente quanto enviar uma mensagem e receber de forma instantânea tudo o que você precisa para seus estudos.</p>
+                    <p>Agora é possível com o Enem Guru. A preparação para o Enem nunca foi tão conveniente quanto enviar uma mensagem e receber de forma instantânea tudo o que você precisa para seus estudos.</p>
                 </div>
             </section>
 
             <section id="features">
                 <div className="content">
                     <h3>O que faz?</h3>
-                    <h2>O que você pode fazer com a <em>FBAI</em></h2>
+                    <h2>O que você pode fazer com o <em>Enem Guru</em></h2>
                     <p>Confira algumas das funcionalidades disponíveis para você.</p>
                 </div>
                 <div className="list">
                     <div className="item">
                         {/*<i className="icon">Icon</i>*/}
                         <h4>Resumos Instantâneos</h4>
-                        <p>FBAI produz resumos detalhados e precisos de tópicos específicos em questão de segundos.</p>
+                        <p>Enem Guru produz resumos detalhados e precisos de tópicos específicos em questão de segundos.</p>
                     </div>
                     <div className="item">
                         {/*<i className="icon">Icon</i>*/}
                         <h4>Respostas Comentadas</h4>
-                        <p>Com a FBAI, obtenha respostas comentadas que enriquecem sua compreensão do assunto.</p>
+                        <p>Com o Enem Guru, obtenha respostas comentadas que enriquecem sua compreensão do assunto.</p>
                     </div>
                     <div className="item">
                         {/*<i className="icon">Icon</i>*/}
                         <h4>Geração de Quiz</h4>
-                        <p>FBAI é capaz de produzir quizzes para você testar seus conhecimentos.</p>
+                        <p>Enem Guru é capaz de produzir quizzes para você testar seus conhecimentos.</p>
                     </div>
                     <div className="item">
                         {/*<i className="icon">Icon</i>*/}
@@ -114,7 +114,7 @@ export function LandingPage() {
             <section id="closing-argument">
                 <div className="content">
                     <h3>Por que usar?</h3>
-                    <h2>Além disso, a assistente possui uma linguagem acessível e de <em>fácil compreensão</em></h2>
+                    <h2>Além disso, o assistente possui uma linguagem acessível e de <em>fácil compreensão</em></h2>
                     <p>Isso significa que, independentemente do seu nível de conhecimento, você poderá aprender de forma eficaz, sem complicações.</p>
                 </div>
                 <div className="image">
@@ -125,10 +125,10 @@ export function LandingPage() {
             <section id='call-to-action'>
                 <div className="content">
                     <h3>Como usar?</h3>
-                    <h2>Descubra a facilidade de usar a FBAI para sua preparação do Enem</h2>
-                    <p>Inicie a conversa agora mesmo com a nossa assistente de IA e potencialize sua preparação para o Enem. O futuro do seu sucesso acadêmico está a apenas uma mensagem de distância.</p>
+                    <h2>Descubra a facilidade de usar o Enem Guru para sua preparação do Enem</h2>
+                    <p>Inicie a conversa agora mesmo com o nosso assistente de IA e potencialize sua preparação para o Enem. O futuro do seu sucesso acadêmico está a apenas uma mensagem de distância.</p>
                     <div className="cta-btns">
-                        <a href="https://api.whatsapp.com/send?phone=558588686378" target='blank'><button className="primary-btn">Experimente agora</button></a>
+                        <a href="https://api.whatsapp.com/send?phone=5585988686378&text=Olá%20Guru!%20Me%20passa%20umas%20dicas%20sobre%20o%20que%20eu%20devo%20estudar%20nessa%20reta%20final%20para%20o%20ENEM!%20" target='blank'><button className="primary-btn">Experimente agora</button></a>
                         {/*<button className="secondary-btn">Entre em contato</button>*/}
                     </div>
                 </div>
@@ -149,24 +149,24 @@ export function LandingPage() {
                 </div>
                 <div className="divisor"></div>
                 <div className="copy-and-social">
-                    <p>Copyright &copy; 2023 Sistema Farias Brito </p>
+                    <p>Copyright &copy; 2023. NeoCode Solutions </p>
                     <nav className="social">
                         <ul>
                             <li>
-                                <a href="https://www.instagram.com/fariasbrito" target='_blank'>
+                                <a href="https://www.instagram.com/neocodesolutions/" target='_blank'>
                                     <FaInstagram />
                                 </a>
                             </li>
-                            <li>
+                            {/**<li>
                                 <a href="#" target='_blank'>
                                     <FaFacebookF />
                                 </a>
-                            </li>
+                            </li>*/}
                             <li>
-                                <a href="#" target='_blank'>
+                                <a href="https://www.linkedin.com/company/neo-code-solutions/" target='_blank'>
                                     <FaLinkedinIn />
                                 </a>
-                            </li>
+                            </li>{/* 
                             <li>
                                 <a href="#" target='_blank'>
                                     <FaTwitter />
@@ -176,7 +176,7 @@ export function LandingPage() {
                                 <a href="#" target='_blank'>
                                     <FaYoutube />
                                 </a>
-                            </li>
+                            </li>*/}
                         </ul>
                     </nav>
                 </div>
